001438d4973b69db8977a189fb68300bec1f11c2
Manuel Cataldo <manuelcataldo51@gmail.com>
2024-10-21 12:26:47 +0200

    Created Files for Lab2

---
+++ Lab2/exercise_set4.py
@@ -0,0 +1,59 @@
+"""Exercise Set 4: Pandas & Matplotlib"""
+
+import numpy as np
+import matplotlib as plt
+
+
+# Please, remove all the pass in the exercises and substitute them with the expected methods for your functions
+
+
+# ex1
+def exercise1():
+    pass
+
+
+# ex2
+def exercise2():
+    pass
+
+
+# ex3
+def exercise3():
+    pass
+
+
+# ex4
+def exercise4():
+    pass
+
+
+# ex5
+def exercise5():
+    pass
+
+# ex6
+def exercise6():
+    pass
+
+
+# ex7
+def exercise7():
+    pass
+
+
+if __name__ == "__main__":
+    print("EXERCISE SET 4")
+    print("EX1")
+    exercise1()
+    print("EX2")
+    exercise2()
+    print("EX3")
+    exercise3()
+    print("EX4")
+    exercise4()
+    print("EX5")
+    exercise5()
+    print("EX6")
+    exercise6()
+    print("EX7")
+    exercise7()
---
+++ Lab2/exercise_set5.py
@@ -0,0 +1,46 @@
+"""Exercise Set 5: JSON"""
+
+import numpy as np
+import matplotlib as plt
+
+
+# Please, remove all the pass in the exercises and substitute them with the expected methods for your functions
+
+
+# ex1
+def exercise1():
+    pass
+
+
+# ex2
+def exercise2():
+    pass
+
+
+# ex3
+def exercise3():
+    pass
+
+
+# ex4
+def exercise4():
+    pass
+
+
+# ex5
+def exercise5():
+    pass
+
+
+if __name__ == "__main__":
+    print("EXERCISE SET 5")
+    print("EX1")
+    exercise1()
+    print("EX2")
+    exercise2()
+    print("EX3")
+    exercise3()
+    print("EX4")
+    exercise4()
+    print("EX5")
+    exercise5()
