09d9a1d83d28426efbf282fe05806abaec9054f6
Manuel Cataldo <manuelcataldo51@gmail.com>
2024-10-21 14:05:27 +0200

    Exercise set 5 done

--- Lab2/exercise_set5.py
+++ Lab2/exercise_set5.py
@@ -2,34 +2,53 @@
 
 import numpy as np
 import matplotlib as plt
-
-
+import json
+#json_obj = '{" Name ": " David ", " Class ": "I", "Age ": 6}'
+#python_obj = {'name':'David','class':'I','age':6}
 # Please, remove all the pass in the exercises and substitute them with the expected methods for your functions
 
 
 # ex1
 def exercise1():
-    pass
-
+    json_obj = '{" Name ": " David ", " Class ": "I", "Age ": 6}'
+    dic = json.loads(json_obj)
+    print("Dictionary: ", dic)
 
 # ex2
 def exercise2():
-    pass
+    python_obj = {'name': 'David', 'class': 'I', 'age': 6}
+    json_obj = json.dumps(python_obj, indent=2)
+    print("JSON Data: ")
+    print(json_obj)
 
 
 # ex3
 def exercise3():
-    pass
+    python_obj = {'name': 'David', 'class': 'I', 'age': 6}
+    json_str = json.dumps(python_obj)
+    print("JSON String: ", json_str)
 
 
 # ex4
 def exercise4():
-    pass
+    python_obj = {'name': 'David', 'class': 'I', 'age': 6}
+    json_obj = json.dumps(python_obj, sort_keys=True, indent=2)
+    print("JSON Data:")
+    print(json_obj)
 
 
 # ex5
 def exercise5():
-    pass
+    file = open('../states.json', 'r')
+    data = json.load(file)
+    file.close()
+    for state in data["states"]:
+        if "area_codes" in state:
+            del state["area_codes"]
+    new_file = open('../statesNAC.json', 'w')
+    json.dump(data, new_file, indent=2)
+    new_file.close()
+    print("New JSON file created without the 'area_codes' field.")
 
 
 if __name__ == "__main__":
