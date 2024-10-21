a0605a446671d14eb876893d1ebc56fb00b2e16b
Manuel Cataldo <manuelcataldo51@gmail.com>
2024-10-21 13:32:15 +0200

    Exercise set 4 done

--- Lab2/exercise_set4.py
+++ Lab2/exercise_set4.py
@@ -1,7 +1,8 @@
 """Exercise Set 4: Pandas & Matplotlib"""
 
 import numpy as np
-import matplotlib as plt
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 # Please, remove all the pass in the exercises and substitute them with the expected methods for your functions
@@ -9,36 +10,79 @@ import matplotlib as plt
 
 # ex1
 def exercise1():
-    pass
+    df = pd.read_csv("../sales_data.csv")
+    plt.plot(df['month_number'], df['total_profit'])
+    plt.show()
 
 
 # ex2
 def exercise2():
-    pass
+    df = pd.read_csv("../sales_data.csv")
+    plt.plot(df['month_number'], df['total_profit'], color='r', marker='o', markerfacecolor='k', linestyle='-', linewidth=3)
+    plt.title('Profile data of last year')
+    plt.show()
 
 
 # ex3
 def exercise3():
-    pass
+    df = pd.read_csv("../sales_data.csv")
+    plt.plot(df['month_number'], df['facecream'], label='Facecream', marker='o')
+    plt.plot(df['month_number'], df['facewash'], label='Facewash', marker='o')
+    plt.plot(df['month_number'], df['toothpaste'], label='Toothpaste', marker='o')
+    plt.plot(df['month_number'], df['bathingsoap'], label='Bathing Soap', marker='o')
+    plt.plot(df['month_number'], df['shampoo'], label='Shampoo', marker='o')
+    plt.plot(df['month_number'], df['moisturizer'], label='Moisturizer', marker='o')
+    plt.xlabel('Month')
+    plt.ylabel('Sales')
+    plt.title('Product Sales Data')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
 
 # ex4
 def exercise4():
-    pass
+    df = pd.read_csv("../sales_data.csv")
+    plt.scatter(df['month_number'], df['toothpaste'])
+    plt.xlabel('Month')
+    plt.ylabel('Toothpaste sales')
+    plt.title('Toothpaste sales over a year')
+    plt.grid(True)
+    plt.show()
 
 
 # ex5
 def exercise5():
-    pass
+    df = pd.read_csv("../sales_data.csv")
+    plt.bar(df['month_number'], df['bathingsoap'])
+    plt.xlabel('Month')
+    plt.ylabel('Bathing soap sales')
+    plt.title('Bathing soap sales over a year')
+    plt.grid(True)
+    plt.savefig('bathingsoapBar.png')
+    plt.show()
 
 # ex6
 def exercise6():
-    pass
+    df = pd.read_csv("../sales_data.csv")
+    plt.hist(df['total_profit'], edgecolor='black')
+    plt.title('All sales over a year')
+    plt.grid(True)
+    plt.show()
 
 
 # ex7
 def exercise7():
-    pass
+    df = pd.read_csv("../sales_data.csv")
+    plt.subplot(121)
+    plt.plot(df['month_number'], df['facewash'], marker='o')
+    plt.title('Facewash sales over a year')
+    plt.grid(True)
+    plt.subplot(122)
+    plt.plot(df['month_number'], df['bathingsoap'], marker='o')
+    plt.title('Bathing soap sales over a year')
+    plt.grid(True)
+    plt.show()
 
 
 if __name__ == "__main__":
